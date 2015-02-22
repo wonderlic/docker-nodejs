d2622b8385870ea995e58714b5de7a8f82203671
Stephen Erisman <stephen.erisman@wonderlic.com>
2015-02-21 19:22:44 -0600

    Move v0.10.36 to sub-directory and add in v0.12.0

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+cd 0.10.36
+
 cp /lib/x86_64-linux-gnu/libgcc_s.so.1 .
 cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.19 .
 
@@ -9,3 +11,19 @@ mv node-v0.10.36-linux-x64/bin/node .
 rm -rf node-v0.10.36-linux-x64
 
 docker build -t wonderlic/nodejs:0.10.36 .
+
+cd ..
+cd 0.12.0
+
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.19 .
+
+if [ ! -f node-v0.12.0-linux-x64.tar.gz ]; then
+    wget http://nodejs.org/dist/v0.12.0/node-v0.12.0-linux-x64.tar.gz
+fi
+tar -zxf node-v0.12.0-linux-x64.tar.gz node-v0.12.0-linux-x64/bin/node
+mv node-v0.12.0-linux-x64/bin/node .
+rm -rf node-v0.12.0-linux-x64
+
+docker build -t wonderlic/nodejs:0.12.0 .
+docker build -t wonderlic/nodejs:latest .
