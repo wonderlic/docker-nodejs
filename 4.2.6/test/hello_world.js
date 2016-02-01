5f575542dfbba6e055c6032c5a2fc71536b4b592
Stephen Erisman <stephen.erisman@wonderlic.com>
2016-02-01 15:58:16 -0600

    Added Node 4.2.6, added test for each Node version, and upgraded lib files from upgraded ubuntu 14.04.03 server

---
+++ 4.2.6/test/hello_world.js
@@ -0,0 +1,1 @@
+console.log('---------- Hello from node', process.versions.node, '----------');
--- build.sh
+++ build.sh
@@ -12,6 +12,11 @@ rm -rf node-v0.10.36-linux-x64
 
 docker build -t wonderlic/nodejs:0.10.36 .
 
+cd test
+docker build -t wonderlic/hello_world:0.10.36 .
+docker run wonderlic/hello_world:0.10.36
+cd ..
+
 cd ..
 cd 0.12.0
 
@@ -26,4 +31,31 @@ mv node-v0.12.0-linux-x64/bin/node .
 rm -rf node-v0.12.0-linux-x64
 
 docker build -t wonderlic/nodejs:0.12.0 .
+
+cd test
+docker build -t wonderlic/hello_world:0.12.0 .
+docker run wonderlic/hello_world:0.12.0
+cd ..
+
+cd ..
+cd 4.2.6
+
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.19 .
+
+if [ ! -f node-v4.2.6-linux-x64.tar.gz ]; then
+    wget http://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-x64.tar.gz
+fi
+tar -zxf node-v4.2.6-linux-x64.tar.gz node-v4.2.6-linux-x64/bin/node
+mv node-v4.2.6-linux-x64/bin/node .
+rm -rf node-v4.2.6-linux-x64
+
+docker build -t wonderlic/nodejs:4.2.6 .
+
+cd test
+docker build -t wonderlic/hello_world:4.2.6 .
+docker run wonderlic/hello_world:4.2.6
+cd ..
+
 docker build -t wonderlic/nodejs:latest .
+
