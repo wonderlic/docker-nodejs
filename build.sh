f5428758e8b112a08db5938d49a66705de7bfa7c
Stephen Erisman <stephen.erisman@wonderlic.com>
2016-05-03 09:57:23 -0500

    Add Node 4.4.3

--- build.sh
+++ build.sh
@@ -77,6 +77,25 @@ docker build -t wonderlic/hello_world:4.3.0 .
 docker run wonderlic/hello_world:4.3.0
 cd ..
 
+cd ..
+cd 4.4.3
+
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.19 .
+
+if [ ! -f node-v4.4.3-linux-x64.tar.gz ]; then
+    wget http://nodejs.org/dist/v4.4.3/node-v4.4.3-linux-x64.tar.gz
+fi
+tar -zxf node-v4.4.3-linux-x64.tar.gz node-v4.4.3-linux-x64/bin/node
+mv node-v4.4.3-linux-x64/bin/node .
+rm -rf node-v4.4.3-linux-x64
+
+docker build -t wonderlic/nodejs:4.4.3 .
+
+cd test
+docker build -t wonderlic/hello_world:4.4.3 .
+docker run wonderlic/hello_world:4.4.3
+cd ..
 
 docker build -t wonderlic/nodejs:latest .
 
