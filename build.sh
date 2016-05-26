66ad3ea2221e7eccb8bd6af79ecd6fecd5558b56
Stephen Erisman <stephen.erisman@wonderlic.com>
2016-05-26 09:03:37 -0500

    Add Node 4.4.5

--- build.sh
+++ build.sh
@@ -17,6 +17,8 @@ docker build -t wonderlic/hello_world:0.10.36 .
 docker run wonderlic/hello_world:0.10.36
 cd ..
 
+#-----------------------------------------------------------------------
+
 cd ..
 cd 0.12.0
 
@@ -36,6 +38,8 @@ cd test
 docker build -t wonderlic/hello_world:0.12.0 .
 docker run wonderlic/hello_world:0.12.0
 cd ..
+
+#-----------------------------------------------------------------------
 
 cd ..
 cd 4.2.6
@@ -57,6 +61,8 @@ docker build -t wonderlic/hello_world:4.2.6 .
 docker run wonderlic/hello_world:4.2.6
 cd ..
 
+#-----------------------------------------------------------------------
+
 cd ..
 cd 4.3.0
 
@@ -77,6 +83,8 @@ docker build -t wonderlic/hello_world:4.3.0 .
 docker run wonderlic/hello_world:4.3.0
 cd ..
 
+#-----------------------------------------------------------------------
+
 cd ..
 cd 4.4.3
 
@@ -96,6 +104,29 @@ cd test
 docker build -t wonderlic/hello_world:4.4.3 .
 docker run wonderlic/hello_world:4.4.3
 cd ..
+
+#-----------------------------------------------------------------------
+
+cd ..
+cd 4.4.5
+
+cp /lib/x86_64-linux-gnu/libgcc_s.so.1 .
+cp /usr/lib/x86_64-linux-gnu/libstdc++.so.6.0.19 .
+
+if [ ! -f node-v4.4.5-linux-x64.tar.gz ]; then
+    wget http://nodejs.org/dist/v4.4.5/node-v4.4.5-linux-x64.tar.gz
+fi
+tar -zxf node-v4.4.5-linux-x64.tar.gz node-v4.4.5-linux-x64/bin/node
+mv node-v4.4.5-linux-x64/bin/node .
+rm -rf node-v4.4.5-linux-x64
+
+docker build -t wonderlic/nodejs:4.4.5 .
+
+cd test
+docker build -t wonderlic/hello_world:4.4.5 .
+docker run wonderlic/hello_world:4.4.5
+cd ..
+
 
 docker build -t wonderlic/nodejs:latest .
 
